a3bddd3b1a641b7f97f901d575acb96662b9e54a
kevin9889nivek <russokevin98@gmail.com>
2017-03-06 15:55:06 +0100

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,66 +3,130 @@
 #include <ESP8266mDNS.h>
 #include <ArduinoOTA.h>
 #include <ESPAsyncTCP.h>
-#include <ESPAsyncTCPbuffer.h>
 
 #define LEDPin D4
 
-const char* ssid= "pino";
-const char* password= "password";
 
-AsyncClient client1;
-
-IPAddress ip(192,168,2,100);
+const char *ssid = "TNCAP993B7D";
+const char *password = "DFC3B98A7A";
 
 
+const char *ap_ssid = "pino";
+const char *ap_password = "password";
+
+const char *hostName = "pino";
+
+const int port = 3000;
+const char maxClients = 7;
+
+
+
+AsyncClient *clients[maxClients] = {NULL};
+
+AsyncServer server(port);
+
+
+IPAddress ip(192, 168, 2, 100);
+IPAddress gateway(192, 168, 2, 254);
+IPAddress subnet(255, 255, 255, 0);
+
+
+uint8_t toInt(unsigned char numero) {
+    return (numero - 48);
+}
+
+unsigned char firstEmpty(AsyncClient **pointer) {
+    unsigned char i = 0;
+    while (pointer[i] != NULL) {
+        Serial.printf("ptr: %p\n", pointer[i]);
+        i++;
+    }
+    return i;
+}
 
 void setup() {
     // put your setup code here, to run once:
-
+    randomSeed(analogRead(0));
     Serial.begin(115200);
     Serial.setDebugOutput(true);
 
-    WiFi.mode(WIFI_STA);
+    WiFi.mode(WIFI_AP_STA);
 
     WiFi.begin(ssid, password);
 
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.printf("STA: Failed to connect!\nSTA: will retry in 1s...\n");
-        WiFi.disconnect();
-        delay(1000);
-        WiFi.begin(ssid, password);
+    WiFi.softAP(ap_ssid, ap_password);
+
+    WiFi.softAPConfig(ip, gateway, subnet);
+
+
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("Connection Failed! Rebooting...");
+        delay(5000);
+        ESP.restart();
     }
+    ArduinoOTA.begin();
+    MDNS.begin(hostName);
+    MDNS.addService("my-service", "tcp", port);
+    server.onClient(
+            [](void *server, AsyncClient *client) {
+                uint8_t *number = (uint8_t *) malloc(sizeof(uint8_t));
+                *number = firstEmpty(clients);
+                clients[*number] = client;
+                Serial.printf("TCP: Client %i connected\n", *number);
 
-    client1.connect(ip,3000);
-    client1.onConnect(
-            [](void* client, AsyncClient* cli){
-                cli->write("1");
-                Serial.println("1");
-
-                cli->onData(
+                client->onData(
                         [](void *number, AsyncClient *client, void *data, size_t len) {
-                            if (((char *) data)[0] == '2') {
-                                digitalWrite(LEDPin,HIGH);
-                                Serial.println("Led acceso");
-
+                            if (((char *) data)[0] == ':') {
+                                if (clients[toInt(((char *) data)[1])] == NULL) {
+                                    return client->write("error: idiot\n");
+                                }
+                                clients[toInt(((char *) data)[1])]->write(((char *) data));
                             }
-
+                            Serial.printf("Data from %i: ", *(char *) number);
                             for (unsigned char i = 0; i < len; ++i) {
 
                                 Serial.print(((char *) data)[i]);
                             }
 
                             Serial.println();
-                        },NULL
+                        }, number
                 );
-            },NULL);
+                client->onDisconnect([](void *number, AsyncClient *client) {
+                    client->~AsyncClient();
+                    clients[*(char *) number] = NULL;
+                }, number);
+            }, NULL);
 
-
-
-
+    server.begin();
 }
+
 
 void loop() {
     // put your main code here, to run repeatedly:
+    //ArduinoOTA.handle();
+    delay(5000);
+
+    int randomclient,prevrandomclient;
+
+    do {
+        randomclient = random(firstEmpty(clients)+1);
+    }while(randomclient==prevrandomclient);
+    Serial.println(randomclient);
+
+    if(clients[randomclient]!=NULL)
+    {
+        if(randomclient == firstEmpty(clients) + 1)
+        {
+            digitalWrite(LEDPin,HIGH);
+            delay(5000);
+            digitalWrite(LEDPin,LOW);
+        }
+        clients[randomclient]->write("2");
+        Serial.println("Dati inviato");
+    }
+    prevrandomclient=randomclient;
+    Serial.println("2");
+    delay(5000);
+
 
 }
